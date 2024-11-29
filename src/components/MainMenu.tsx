221ee4a36cd1885f2a0f867b4393400e0595e2c4
Lai Carson <laicarson.swe@gmail.com>
2024-11-29 16:27:05 +0800

    refactor(menulink): Change MenuLink width and height to be dynamic accoring to its content
    Change width and height to min-width and min-height to allow component to expand if link is long

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,7 +1,12 @@
 import MenuLink from "./menuLinks/menuLink";
 
 function MainMenu() {
-  return <MenuLink text="New Game" path="/game" />;
+  return (
+    <>
+      <MenuLink text="New Game" path="/game" />
+      {/* TODO: Add "Continue Game button" */}
+    </>
+  );
 }
 
 export default MainMenu;
