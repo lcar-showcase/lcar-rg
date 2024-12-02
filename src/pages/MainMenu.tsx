2974e56548a1c5f4de21ad88875b44546247601a
Lai Carson <laicarson.swe@gmail.com>
2024-12-02 14:33:59 +0800

    refactor(board): Change page container to occupy full view height
    Change root element to use flexbox

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,11 +1,13 @@
 import MenuLink from "../components/menuLinks/menuLink";
+import style from "./mainMenu.module.css";
 
 function MainMenu() {
   return (
-    <>
-      <MenuLink text="New Game" path="/game" />
-      {/* TODO: Add "Continue Game button" */}
-    </>
+    <div className={style.container}>
+        <MenuLink text="New Game" path="/game" />
+        {/* TODO: Add "Continue Game button" */}
+        <MenuLink text="Continue Game" path="/game" />
+      </div>
   );
 }
 
