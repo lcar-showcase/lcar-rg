24b54908e3808efbac108e25574d514fe701b513
Lai Carson <laicarson.swe@gmail.com>
2024-12-03 09:53:04 +0800

    docs(board): Add documentation in board.tsx

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -2,26 +2,43 @@ import Tile, { TilePos } from "../tile/tile";
 import style from "./board.module.css";
 import { initTileLabels, getRows, getCols } from "./tileLabels";
 
-// Initialise all tile labels
+// Initialise all 64 Tile labels
 const tileLabels = initTileLabels();
 
-function initDefaultDisk(tile: TilePos) {
-  if (`${tile.row}${tile.col}` === "4E" || `${tile.row}${tile.col}` === "5D") {
-    // Initialise dark disk on tile
-    return <Tile key={`${tile.row}${tile.col}`} pos={{ row: tile.row, col: tile.col }} diskColour="dark" />;
-  } if (`${tile.row}${tile.col}` === "4D" || `${tile.row}${tile.col}` === "5E") {
-    // Initialise light disk on tile
-    return <Tile key={`${tile.row}${tile.col}`} pos={{ row: tile.row, col: tile.col }} diskColour="light" />;
-  } 
-    // No disk on tile
-    return <Tile key={`${tile.row}${tile.col}`} pos={{ row: tile.row, col: tile.col }} diskColour={null} />;
-  
+/**
+ * Callback to initialise all Tile components, and add default disks to Tiles.
+ *
+ * @param tile - Position of the Tile, defined by row and col.
+ * @returns A Tile component, with an optional disk colour specified.
+ */
+function initDefaultDisk(tilePos: TilePos) {
+  const tileRow = tilePos.row;
+  const tileCol = tilePos.col;
+
+  if (`${tileRow}${tileCol}` === "4E" || `${tileRow}${tileCol}` === "5D") {
+    // Initialise dark disk on Tile
+    return <Tile key={`${tileRow}${tileCol}`} pos={{ row: tileRow, col: tileCol }} diskColour="dark" />;
+  }
+  if (`${tileRow}${tileCol}` === "4D" || `${tileRow}${tileCol}` === "5E") {
+    // Initialise light disk on Tile
+    return <Tile key={`${tileRow}${tileCol}`} pos={{ row: tileRow, col: tileCol }} diskColour="light" />;
+  }
+  // No disk
+  return <Tile key={`${tileRow}${tileCol}`} pos={{ row: tileRow, col: tileCol }} diskColour={null} />;
 }
 
+/**
+ * Initialise Tiles components from all 64 Tile labels.
+ * @returns Tile components.
+ */
 function initTiles() {
   return tileLabels.map(initDefaultDisk);
 }
 
+/**
+ * Initialise row labels to the left of the board.
+ * @returns Row labels.
+ */
 function initRowLabels() {
   return getRows().map((row) => (
     <div key={row} className={style.label}>
@@ -30,6 +47,10 @@ function initRowLabels() {
   ));
 }
 
+/**
+ * Initialise column labels to the top of the board.
+ * @returns Column labels.
+ */
 function initColLabels() {
   return getCols().map((col) => (
     <div key={col} className={style.label}>
@@ -38,13 +59,14 @@ function initColLabels() {
   ));
 }
 
-// Initialise board labels (side labels)
 const rowLabels = initRowLabels();
 const colLabels = initColLabels();
-
-// Initialise board tiles with default disks
 const tiles = initTiles();
 
+/**
+ * The game board.
+ * @returns Board component.
+ */
 function Board() {
   return (
     <section className={style.container}>
