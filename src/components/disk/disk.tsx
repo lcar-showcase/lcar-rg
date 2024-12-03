737aeb6319b2d33e425f995b8e060201508178a4
Lai Carson <laicarson.swe@gmail.com>
2024-12-03 10:02:28 +0800

    docs(disk): Add documentation for disk.tsx

--- src/components/disk/disk.tsx
+++ src/components/disk/disk.tsx
@@ -2,11 +2,16 @@ import style from "./disk.module.css";
 
 export type DiskColour = "dark" | "light" | null;
 
+/**
+ * A coloured disk.
+ * @param colour - Disk colour.
+ * @returns Disk component.
+ */
 function Disk({ colour }: { colour: DiskColour }) {
   if (colour) {
     return <div className={colour === "dark" ? `${style.disk} ${style.dark}` : `${style.disk} ${style.light}`} />;
   }
-  return null; // No disk; empty tile
+  return null; // No disk colour given; empty tile
 }
 
 export default Disk;
