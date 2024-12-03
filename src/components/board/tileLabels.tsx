5ee69adb022ad1ffc411952c0faa066ce1bafee8
Lai Carson <laicarson.swe@gmail.com>
2024-12-03 09:58:16 +0800

    docs(tile): Add documentation for tileLabels.tsx

--- src/components/board/tileLabels.tsx
+++ src/components/board/tileLabels.tsx
@@ -1,21 +1,31 @@
-interface TileLabel {
-  row: string;
-  col: string;
-}
+import { TilePos } from "../tile/tile";
 
+// All possible rows and column labels on the board
 const rows = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const cols = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
+/**
+ * Getter for all row labels.
+ * @returns Row labels.
+ */
 export function getRows() {
   return rows;
 }
 
+/**
+ * Getter for all column labels.
+ * @returns Column labels.
+ */
 export function getCols() {
   return cols;
 }
 
+/**
+ * Create Tile labels, based on all combinations of row and column labels.
+ * @returns All 64 Tile labels.
+ */
 export function initTileLabels() {
-  const labels: TileLabel[] = []; // All 64 tile labels
+  const labels: TilePos[] = [];
 
   rows.forEach((row) => {
     cols.forEach((col) => {
