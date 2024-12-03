c2dd77fd544729567b06284fa2a7d4732de55132
Lai Carson <laicarson.swe@gmail.com>
2024-12-03 10:04:21 +0800

    docs(logo): Add documentation for logo.tsx

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router";
 import style from "./logo.module.css";
 
+/**
+ * The game logo.
+ * @returns A Logo component.
+ */
 function Logo() {
   return (
     <Link to="/" className={`${style.text} ${style.navigable}`}>
