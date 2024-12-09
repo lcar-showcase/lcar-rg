0804f879838485fdb2fc8c2d16540569b16d2a0e
Lai Carson <laicarson.swe@gmail.com>
2024-12-09 10:08:25 +0800

    refactor(game): Put boardArray inside useState

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,24 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router";
 import Board from "../../components/board";
 import { DiskColour } from "../../types";
 import style from "./game.module.css";
 
 // Initialise 8 by 8 board, no disks on each tile
-const boardArray: DiskColour[][] = Array.from({ length: 8 }, () => Array.from({ length: 8 }));
+const initBoardArray: DiskColour[][] = Array.from({ length: 8 }, () => Array.from({ length: 8 }));
 
-// Set default disks
-boardArray[3][3] = "light";
-boardArray[4][4] = "light";
-boardArray[3][4] = "dark";
-boardArray[4][3] = "dark";
+// Initialise default disks
+// TODO: Add configurations in a separate file to test the game in the future
+initBoardArray[3][3] = "light";
+initBoardArray[4][4] = "light";
+initBoardArray[3][4] = "dark";
+initBoardArray[4][3] = "dark";
 
 function Game() {
+  // * Not defining a set function yet, as I'm not using it and ESLint is complaining
+  const [boardArr] = useState(initBoardArray);
+
   return (
     <>
       <Link to="/" className={style.logo}>
         REVERSI
       </Link>
-      <Board boardArray={boardArray} />
+      <Board boardArray={boardArr} />
     </>
   );
 }
