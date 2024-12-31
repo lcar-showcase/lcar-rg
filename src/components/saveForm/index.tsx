4c351f8907ad3c506169370cfa96c8e0d4c1b048
Lai Carson <laicarson.swe@gmail.com>
2024-12-31 10:20:35 +0800

    feat(saveform): Add regular expression checking for user input when entering save name
    Input must be alphanumeric, with no spaces, and at least 1 character long

--- src/components/saveForm/index.tsx
+++ src/components/saveForm/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { PopUpType } from "../../types";
 import style from "./saveForm.module.css";
@@ -9,18 +9,26 @@ interface FormProps {
 }
 
 function CustomForm({ formType, disablePopUp }: FormProps) {
+  const [currentName, setCurrentName] = useState(""); // Current text input
+  const [errorMsg, setErrorMsg] = useState(formType === "save" ? "Save name taken." : "Save name not found.");
   const label = formType === "save" ? "Enter a save name" : "Enter an existing save name";
   const id = "gameid";
   const name = "gameid";
-  const placeholder = "Letters and numbers only; maximum 20 characters";
-  const errorMsg = formType === "save" ? "Save name taken." : "Save name not found.";
+  const placeholder = "Letters and numbers only; 1-20 characters";
   const goTo = useNavigate();
 
   // TODO: Pass pop-up type here, then handle the logic for save/continue here
-  const handleFormSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    disablePopUp();
-    goTo("/game");
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Alphanumeric, no spaces
+    const regex = /^[a-zA-Z0-9]+$/; // TODO: Test regex more extensively during refinement
+    // Check if inputs valid (alphanumeric only)
+    if (!regex.exec(currentName)) {
+      setErrorMsg("Invalid name provided.");
+    } else {
+      disablePopUp();
+      goTo("/game");
+    }
   };
 
   return (
@@ -29,7 +37,16 @@ function CustomForm({ formType, disablePopUp }: FormProps) {
       <div className={style.nameInputContainer}>
         <label htmlFor={name}>
           {label}
-          <input type="text" id={id} name={name} placeholder={placeholder} />
+          <input
+            type="text"
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            maxLength={20}
+            onChange={(e) => {
+              setCurrentName(e.target.value);
+            }}
+          />
         </label>
         <p>{errorMsg}</p>
       </div>
