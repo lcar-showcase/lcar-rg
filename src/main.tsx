2ac24ef0cdb7e26e31e2d51592564737ccf42a06
Lai Carson <laicarson.swe@gmail.com>
2025-01-14 15:44:59 +0800

    fix: Add base attribute in Vite config and change BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Game from "./pages/game";
 import MainMenu from "./pages/mainMenu";
@@ -8,12 +8,12 @@ import PageNotFound from "./pages/pageNotFound";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<MainMenu />} />
         <Route path="/game" element={<Game />} />
         <Route path="*" element={<PageNotFound />} /> {/* Invalid path */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
